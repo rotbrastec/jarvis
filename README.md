7f94b3b73ab0d0a03801712f75ea2133d3d2a4f7
rotbrastec <95126867+rotbrastec@users.noreply.github.com>
2021-12-30 08:40:18 -0300

    ggd

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # jarvis
  um assistente virtual feito em python
+
+foco:
+    facil de aprender.
+    Rapido e expansivel.
+Tecnologias:
+    reconhecimento de voz.
+    sintese de voz.
+    algum de tipo IA.
---
+++ main.py
@@ -0,0 +1,9 @@
+#our main file.
+
+import speech_recognition as sr
+# criar um reconhecedor
+r =sr.Recognizer()
+#abrir o microfone para captura
+with sr.Microphone() as source:
+    audio=r.listen(source)#define microfone como fonte de audio
+    print(r.recognize_google(audio))
